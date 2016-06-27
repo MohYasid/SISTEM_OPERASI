861ee35d632dd48f057f9097a429be28af2f9b84
Mohyasid <moh.yasid.5012@gmail.com>
2016-06-27 13:42:06 +0700

    TUGAS SO

---
+++ Luas & Kllng Lgkan/unit1.pas
@@ -0,0 +1,55 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+  var r,L,K,phi:Real;
+
+begin
+     phi:=3.14;
+     r:=strtofloat(edit1.Text);
+     L:=phi*r*r;
+     K:=2*phi*r;
+     label3.Caption:=FloatToStr(L)+ ' ' +edit2.Text;
+     label5.Caption:=FloatTostr(K)+ ' ' +edit2.Text;
+     label3.Visible:=True;
+     label5.Visible:=True;
+end;
+
+end.
+
