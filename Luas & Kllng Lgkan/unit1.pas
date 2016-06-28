cc2cea78834879a3958942c496f97258286453ee
Mohyasid <moh.yasid.5012@gmail.com>
2016-06-28 11:33:44 +0700

    <edit button1>

--- Luas & Kllng Lgkan/unit1.pas
+++ Luas & Kllng Lgkan/unit1.pas
@@ -12,15 +12,16 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button0: TButton;
     Button1: TButton;
-    Edit1: TEdit;
-    Edit2: TEdit;
-    Label1: TLabel;
-    Label2: TLabel;
-    Label3: TLabel;
-    Label4: TLabel;
-    Label5: TLabel;
-    Label6: TLabel;
+    Edit1:   TEdit;
+    Edit2:   TEdit;
+    Label1:  TLabel;
+    Label2:  TLabel;
+    Label3:  TLabel;
+    Label4:  TLabel;
+    Label5:  TLabel;
+    Label6:  TLabel;
     procedure Button1Click(Sender: TObject);
   private
     { private declarations }
@@ -38,18 +39,18 @@ implementation
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
-  var r,L,K,phi:Real;
+var
+  r, L, K, phi: real;
 
 begin
-     phi:=3.14;
-     r:=strtofloat(edit1.Text);
-     L:=phi*r*r;
-     K:=2*phi*r;
-     label3.Caption:=FloatToStr(L)+ ' ' +edit2.Text;
-     label5.Caption:=FloatTostr(K)+ ' ' +edit2.Text;
-     label3.Visible:=True;
-     label5.Visible:=True;
+  phi := 3.14;
+  r   := strtofloat(edit1.Text);
+  L   := phi * r * r;
+  K   := 2 * phi * r;
+  label3.Caption := FloatToStr(L) + ' ' + edit2.Text;
+  label5.Caption := FloatTostr(K) + ' ' + edit2.Text;
+  label3.Visible := True;
+  label5.Visible := True;
 end;
 
 end.
-
